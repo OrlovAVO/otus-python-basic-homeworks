967dd47338b9859055c70613e1bd09168067720e
Artyom Orlov <sossanaka000@gmail.com>
2024-02-08 04:08:35 +0500

    Add homework_06

---
+++ homework_06/blog/config.py
@@ -0,0 +1,9 @@
+SQLALCHEMY_DATABASE_URI = "postgresql+pg8000://username:password@pg:5432/blog"
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+TESTING = False
+DEBUG = False
+
+SECRET_KEY = "secret"
